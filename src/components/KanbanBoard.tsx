7864e06a7e03d5eb52282309934111ca2dbaef5b
Toheedullah-K3 <toheedullah002@gmail.com>
2025-11-18 14:21:50 +0500

    Dnd Kit added

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import Button from './Button'
 import ColumnContainer from './ColumnContainer';
+import { DndContext } from '@dnd-kit/core';
+import { SortableContext } from '@dnd-kit/sortable';
+
 
 // types import
 import type { Column, Id } from '../types'
@@ -21,7 +24,7 @@ function KanbanBoard() {
         setColumns([...columns, columnsToAdd])
     }
 
-    function deleteColumn(id: Id){
+    function deleteColumn(id: Id) {
         const filteredColumn = columns.filter((col) => col.id !== id)
         setColumns(filteredColumn)
     }
@@ -31,14 +34,18 @@ function KanbanBoard() {
     }
     return (
         <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-10">
-            <div className='m-auto flex gap-4'>
-                <div className='flex gap-4'>
-                    {columns.map(col => (
-                        <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn}/>
-                    ))}
+            <DndContext>
+                <div className='m-auto flex gap-4'>
+                    <div className='flex gap-4'>
+                        <SortableContext items={}>
+                            {columns.map(col => (
+                                <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} />
+                            ))}
+                        </SortableContext>
+                    </div>
+                    <Button onClick={createNewColumn} />
                 </div>
-                <Button onClick={createNewColumn} />
-            </div>
+            </DndContext>
         </div>
 
 
